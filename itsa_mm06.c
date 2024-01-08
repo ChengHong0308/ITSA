a09ba07b5cbf89abbb78d4019bc79941f0a8fe46
ChengHong0308 <155963001+ChengHong0308@users.noreply.github.com>
2024-01-08 23:33:08 +0800

    Add files via upload

---
+++ itsa_mm06.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int x;
+    double ans;
+    while(scanf("%d",&x)!=EOF)
+    {
+        ans=x*1.6;
+        printf("%0.1lf\n",floor((ans*10)+0.5)/10);//四捨五入到小數點後一位(floor:向下取整)
+    }
+}
