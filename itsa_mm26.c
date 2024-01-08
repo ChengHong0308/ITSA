14da777814824517b0049e89efc202b5c29912dc
ChengHong0308 <155963001+ChengHong0308@users.noreply.github.com>
2024-01-09 03:49:49 +0800

    Update itsa_mm26.c

--- itsa_mm26.c
+++ itsa_mm26.c
@@ -5,7 +5,7 @@ int main()
     int x;
     while(scanf("%d",&x)!=EOF)
     {
-        for(int i=1;i<=x;i++)
+        for(int i=1;i<=x;i++)//輸出1*1~x*x的結果
         {
             printf("%d*%d=%d\n",i,i,i*i);
         }
