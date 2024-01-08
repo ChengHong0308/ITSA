8a8ee16ca7514ebc25efe84011f0a043586c0c6c
ChengHong0308 <155963001+ChengHong0308@users.noreply.github.com>
2024-01-08 18:11:41 +0800

    Add files via upload

---
+++ itsa_mm01.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{;
+    float up,down,high,ans;
+    while(scanf("%f",&up)!=EOF)
+    {
+        scanf("%f %f",&down,&high);
+        ans=(up+down)*high/2;
+        printf("Trapezoid area:%.1f\n",ans);
+    }
+}
