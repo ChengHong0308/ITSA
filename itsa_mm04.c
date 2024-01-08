68d1e59ee62fc6d408518875dc177ace88f1c503
ChengHong0308 <155963001+ChengHong0308@users.noreply.github.com>
2024-01-08 18:41:23 +0800

    Add files via upload

---
+++ itsa_mm04.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int x,y;
+    while(scanf("%d",&x)!=EOF)
+    {
+        scanf("%d",&y);
+        printf("%d+%d=%d\n",x,y,x+y);
+        printf("%d*%d=%d\n",x,y,x*y);
+        printf("%d-%d=%d\n",x,y,x-y);
+        if(x%y<0)
+        {
+            if(x/y<0)// -10/3=-3...-1,-10/3=-4...2,題目要求餘數>=0
+            {
+                printf("%d/%d=%d...%d\n",x,y,(x/y)-1,x%y+y);
+            }
+            else// -10/-3=3...-1,-10/-3=4...2,題目要求餘數>=0
+            {
+                printf("%d/%d=%d...%d\n",x,y,(x/y)+1,x%y-y);
+            }
+        }
+        else
+        {
+            printf("%d/%d=%d...%d\n",x,y,x/y,x%y);
+        }
+    }
+}
