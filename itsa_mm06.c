104cd0cf3f5bfe0a1c2bab418e52f03ac694b8f0
ChengHong0308 <155963001+ChengHong0308@users.noreply.github.com>
2024-01-09 00:16:57 +0800

    Update itsa_mm06.c

--- itsa_mm06.c
+++ itsa_mm06.c
@@ -5,9 +5,9 @@ int main()
 {
     int x;
     double ans;
-    while(scanf("%d",&x)!=EOF)
+    while(scanf("%d",&x)!=EOF)//輸入英里
     {
-        ans=x*1.6;
+        ans=x*1.6;//轉換成公里
         printf("%0.1lf\n",floor((ans*10)+0.5)/10);//四捨五入到小數點後一位(floor:向下取整)
     }
 }
