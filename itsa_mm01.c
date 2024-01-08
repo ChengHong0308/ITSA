3107dce8e7347ee47360a8e9c6c86b2f068825b2
ChengHong0308 <155963001+ChengHong0308@users.noreply.github.com>
2024-01-09 00:13:27 +0800

    Update itsa_mm01.c

--- itsa_mm01.c
+++ itsa_mm01.c
@@ -6,7 +6,7 @@ int main()
     while(scanf("%f",&up)!=EOF)
     {
         scanf("%f %f",&down,&high);
-        ans=(up+down)*high/2;
-        printf("Trapezoid area:%.1f\n",ans);//梯形面積公式
+        ans=(up+down)*high/2;//梯形面積公式
+        printf("Trapezoid area:%.1f\n",ans);
     }
 }
