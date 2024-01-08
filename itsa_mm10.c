ee6aa866ffaaa0c707971135c211ac49b1c47d1a
ChengHong0308 <155963001+ChengHong0308@users.noreply.github.com>
2024-01-08 23:58:13 +0800

    Update itsa_mm10.c

--- itsa_mm10.c
+++ itsa_mm10.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <math.h>
 
-int main() {
+int main()
+{
     double c,f;
     while(scanf("%lf",&c)!=EOF)//輸入攝氏
     {
         f=(c*9/5)+32;//轉換成華氏
         printf("%0.1lf\n",f);//輸出四捨五入至小數點第一位的華氏溫度
     }
-    
 }
