143c3a25a930fec481c7c4e642f209a4df802293
ChengHong0308 <155963001+ChengHong0308@users.noreply.github.com>
2024-01-09 03:48:20 +0800

    Update itsa_mm24.c

--- itsa_mm24.c
+++ itsa_mm24.c
@@ -10,15 +10,15 @@ int main()
         scanf("%lf",&y);
         if(x>120)
         {
-            money+=(x-120)*y*1.66;
+            money+=(x-120)*y*1.66;//121小時以上 基本時薪(y)*1.66
             x=120;
         }
         if(x>60)
         {
-            money+=(x-60)*y*1.33;
+            money+=(x-60)*y*1.33;//61~120小時 基本時薪(y)*1.33
             x=60;
         }
-        money+=x*y;
+        money+=x*y;//60小時以下 基本時薪(y)
         printf("%.1lf\n",money);
     }
 }
